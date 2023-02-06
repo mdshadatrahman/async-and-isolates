d974d8abf0744b14548a6f13572201717c6adec0
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-06 12:30:11 +0600

    example4

---
+++ bin/async/example4.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) async {
+  await for (final number in getNumbers()) {
+    print(number);
+  }
+  try {
+    await for (final name in getNames()) {
+      print(name);
+    }
+  } catch (e) {
+    print(e);
+  }
+}
+
+Stream<int> getNumbers() async* {
+  for (int i = 0; i < 10; i++) {
+    await Future.delayed(const Duration(seconds: 1));
+    yield i;
+  }
+}
+
+Stream<String> getNames() async* {
+  await Future.delayed(const Duration(seconds: 1));
+  yield 'Shadat';
+  throw Exception('Something went wrong');
+}

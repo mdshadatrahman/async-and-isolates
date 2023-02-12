cc54c2a5260ac416839c83f3c5ff1b4ccb21fded
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-12 10:28:52 +0600

    example5

--- bin/async/example5.dart
+++ bin/async/example5.dart
@@ -1,1 +1,23 @@
-void main(List<String> args) {}
+void main(List<String> args) async {
+  await for (final character in getNames().asyncExpand((event) => getCharacters(event))) {
+    print(character);
+  }
+
+  await for (final character in getCharacters('fromString')) {
+    print(character);
+  }
+}
+
+Stream<String> getCharacters(String fromString) async* {
+  for (var i = 0; i < fromString.length; i++) {
+    await Future.delayed(const Duration(milliseconds: 300));
+    yield fromString[i];
+  }
+}
+
+Stream<String> getNames() async* {
+  await Future.delayed(const Duration(milliseconds: 200));
+  yield 'John';
+  await Future.delayed(const Duration(milliseconds: 300));
+  yield 'Doe';
+}

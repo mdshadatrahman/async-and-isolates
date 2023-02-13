d0cf5a083969950823f84602d54d17b2094dc07f
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-13 15:01:41 +0600

    ex7

---
+++ bin/async/example7.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) async {
+  await for (final value in numbers()) {
+    print(value);
+  }
+
+  print('__________________________________');
+  await for (final value in numbers(start: 0, end: 10, f: evenNumbersOnly)) {
+    print(value);
+  }
+
+  print('__________________________________');
+  await for (final value in numbers(start: 0, end: 10, f: oddNumbersOnly)) {
+    print(value);
+  }
+}
+
+typedef IsIncluded = bool Function(int value);
+
+bool evenNumbersOnly(int value) => value % 2 == 0;
+bool oddNumbersOnly(int value) => value % 2 != 0;
+
+Stream<int> numbers({
+  int start = 0,
+  int end = 4,
+  IsIncluded? f,
+}) async* {
+  for (var i = start; i < end; i++) {
+    if (f == null || f(i)) {
+      yield i;
+    }
+  }
+}

2a50bc3d4501d1d17550b08711506a9c9e814026
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-28 13:54:41 +0600

    ex16

--- bin/async/example16.dart
+++ bin/async/example16.dart
@@ -1,6 +1,29 @@
 import 'dart:async';
 
-void main(List<String> args) async {}
+void main(List<String> args) async {
+  try {
+    await for (final name in getNames().withTimeoutBetweenEvents(const Duration(seconds: 3))) {
+      print(name);
+    }
+  } on TimeOutBetweenEventsException catch (e, stackTrace) {
+    print('TimeoutBetweenEventsException: $e');
+    print('Stack Trace: $stackTrace');
+  }
+}
+
+Stream<String> getNames() async* {
+  yield 'John';
+  await Future.delayed(const Duration(seconds: 1));
+  yield 'Jane';
+  await Future.delayed(const Duration(seconds: 10));
+  yield 'Doe';
+}
+
+extension WithTimeoutBetweenEvents<T> on Stream<T> {
+  Stream<T> withTimeoutBetweenEvents(Duration duration) => transform(
+        TimeoutBetweenEvents(duration: duration),
+      );
+}
 
 class TimeoutBetweenEvents<E> extends StreamTransformerBase<E, E> {
   final Duration duration;

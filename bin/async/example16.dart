c990887358c2b8e7271d10710b06f8b652ad1dca
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-28 13:43:44 +0600

    ex16

--- bin/async/example16.dart
+++ bin/async/example16.dart
@@ -1,3 +1,43 @@
-void main(List<String> args) async {
-  
+import 'dart:async';
+
+void main(List<String> args) async {}
+
+class TimeoutBetweenEvents<E> extends StreamTransformerBase<E, E> {
+  final Duration duration;
+  const TimeoutBetweenEvents({required this.duration});
+  @override
+  Stream<E> bind(Stream<E> stream) {
+    StreamController<E>? controller;
+    StreamSubscription<E>? subscription;
+    Timer? timer;
+
+    controller = StreamController(
+      onListen: () {
+        subscription = stream.listen(
+          (data) {
+            timer?.cancel();
+            timer = Timer.periodic(
+              duration,
+              (timer) {
+                controller?.addError(TimeOutBetweenEventsException('TimeOut'));
+              },
+            );
+            controller?.add(data);
+          },
+          onError: controller?.addError,
+          onDone: controller?.close,
+        );
+      },
+      onCancel: () {
+        timer!.cancel();
+        subscription!.cancel();
+      },
+    );
+    return controller.stream;
+  }
+}
+
+class TimeOutBetweenEventsException implements Exception {
+  final String message;
+  TimeOutBetweenEventsException(this.message);
 }

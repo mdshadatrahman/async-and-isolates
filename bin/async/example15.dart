37e2d0a18905e709f31796a859f0ef5404b81f35
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-28 13:08:08 +0600

    ex15

--- bin/async/example15.dart
+++ bin/async/example15.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 void main(List<String> args) async {
-  await nonBroadCastStream();
+  // await nonBroadCastStream();
+  await broadCastStream();
 }
 
 Future<void> nonBroadCastStream() async {
@@ -21,4 +22,28 @@ Future<void> nonBroadCastStream() async {
   } catch (e) {
     print(e);
   }
+}
+
+Future<void> broadCastStream() async {
+  late final StreamController<String> controller;
+
+  controller = StreamController<String>.broadcast();
+
+  final sub1 = controller.stream.listen((name) {
+    print('sub1: $name');
+  });
+
+  final sub2 = controller.stream.listen((name) {
+    print('sub2: $name');
+  });
+
+  controller.sink.add('Bob');
+  controller.sink.add('Jack');
+  controller.sink.add('Jill');
+
+  controller.onCancel = () {
+    sub1.cancel();
+    sub2.cancel();
+    print('onCancel');
+  };
 }

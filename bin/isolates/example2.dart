ad80f2bee247cd70a21a8ac7595150cb04fc547e
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-03-15 12:16:42 +0600

    ex2 ongoing

---
+++ bin/isolates/example2.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {}
+
+const messagesAndResponses = {
+  '': 'Ask me a question like "How are you?"',
+  'Hello': 'Hi!',
+  'How are you?': 'I\'m fine, thanks',
+  'What are you doing?': 'Learning about Isolates in Dart',
+  'Are you having fun?': 'Yes, I am',
+};
